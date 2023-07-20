fb8db10832296c753de77fbbe2d193963bd6ddf9
Ciarán Hanrahan <justciaranhanrahan@gmail.com>
2023-07-21 00:08:13 +0100

    nicenineasdad

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,12 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./styles/tailwind.config.js')
   eleventyConfig.addWatchTarget('./styles/tailwind.css')
 
+  eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' })
+
+  eleventyConfig.addCollection("sortedClients", function(collection) {
+    return collection.getFilteredByTag("client").sort((a, b) => (a.data.order > b.data.order) ? 1 : -1);
+  });
+  
   eleventyConfig.addPassthroughCopy({
     './node_modules/alpinejs/dist/cdn.js': './js/alpine.js',
   })
@@ -13,6 +19,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addShortcode('version', function () {
     return now
   })
+
+  eleventyConfig.addPassthroughCopy('src/images');
+  eleventyConfig.addPassthroughCopy('src/fonts');
 
   eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
     if (
